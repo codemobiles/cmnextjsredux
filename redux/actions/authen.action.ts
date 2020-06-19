e41e7891f7d498dd98eec93994bbfdc3c0405252
codemobiles <chaiyasit.t@gmail.com>
2020-06-19 14:59:20 +0700

    สอน NextJS + Redux + Typescript Ep8

--- redux/actions/authen.action.ts
+++ redux/actions/authen.action.ts
@@ -1,15 +1,19 @@
 import { LOGIN } from './../types';
 import {Account} from "../../types/Account"
-export const setLoginReducer = (payload) => ({
+const setLoginReducer = (payload) => ({
     type: LOGIN,
     payload
 })
 
 
-export const login = ({username, password}:Account)=>{
+const login = ({username, password}:Account)=>{
     return dispatch=>{
         setTimeout(()=>{
             dispatch(setLoginReducer({token: "1234", user:{username}}))
         })
     }
 }
+
+export default {
+    login
+};
--- redux/index.ts
+++ redux/index.ts
@@ -1,16 +1,19 @@
-import { LOGIN } from './types';
-const initialState = {
-    token:null,
-    user:null
-}
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { MakeStore, createWrapper, Context } from 'next-redux-wrapper';
 
-export default (state = initialState, { type, payload }) => {
-    switch (type) {
-
-    case LOGIN:
-        return { ...state, token:payload.token, user:{username: payload.user.username} }
-
-    default:
-        return state
+// NOTE: config store
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
     }
 }
+const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+// create a makeStore function
+const makeStore: MakeStore<any> = (context: Context) => createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+// export an assembled wrapper
+export const wrapper = createWrapper<any>(makeStore, { debug: true });
